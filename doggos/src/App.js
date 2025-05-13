f479bc0697dc9ec89f8b01842b3c855b60fc0257
Giovanni Rodriguez <giothedesigner@gmail.com>
2025-05-13 11:53:18 -0700

    initial commit

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import axios from 'axios';
+
+class App extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <>
+                <h1>My App</h1>
+            </>
+        )
+    }
+}
+
+export default App;
